9b59b976c894d4e7297e87d1cc05e7dd9d115aeb
Brendan Mott <bmott1531@gmail.com>
2024-03-26 19:28:17 -0500

    Updated

--- app.js
+++ app.js
@@ -3,10 +3,9 @@
 
 (async () => {
 
-const { prompt } = require('enquirer')
-const axios = require('axios')
+    const survey = require('./survey')
+    survey.setUrl('https://raw.githubusercontent.com/Bmott1977/BIM.SurveyDemo/master/questions.json')
 
-
-
+    console.log(await survey.run())
 
 })()
